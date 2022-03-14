a05260bc2b5e11f961d3c378100dc295e84d3b9c
loyilulu <loyichan2@gmail.com>
2022-03-14 22:35:05 +0100

    added dev-dependency @types/googlemaps for TS checking

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,19 @@
-// Code goes here!
 import axios from "axios";
 
 const API_KEY = "AIzaSyCy7dDyxDuuO9Gr3Fg_cx-BYOcA19b8MAk";
 
 const form = document.querySelector("form")!;
-const addressInput = document.getElementById("id")! as HTMLInputElement;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
 
 type GoogleGeoResponse = {
   results: { geometry: { location: { lat: number; lng: number } } }[];
   status: "OK" | "ZERO_RESULTS";
 };
 
-declare var google any;
-
 function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value;
+  console.log(enteredAddress);
   const googleRequestAPI = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
     enteredAddress
   )}&key=${API_KEY}`;
@@ -27,11 +25,11 @@ function searchAddressHandler(event: Event) {
         throw new Error("Could not fetch the address");
       }
       const coordinates = response.data.results[0].geometry.location;
-      const map = new google.maps.Map(document.getElementById("map"), {
+      const map = new google.maps.Map(document.getElementById("map")!, {
         center: coordinates,
-        zoom: 8,
+        zoom: 16,
       });
-      new google.maps.Marker({position: coordinates, map: map})
+      new google.maps.Marker({ position: coordinates, map: map });
       console.log(coordinates);
     })
     .catch((err) => {
